41288ff373f777330c84c2664b46dd96b28ce943
Alberto Valiente Thoresen <46265925+albertovth@users.noreply.github.com>
2024-07-22 01:34:24 +0200

    Add files via upload

--- init_from_spi_538.py
+++ init_from_spi_538.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
-# Load the 538 SPI data
-url = "https://projects.fivethirtyeight.com/soccer-api/international/spi_global_rankings_intl.csv"
-spi_538_df = pd.read_csv(url)
+# Load the SPI data from the May 25, 2021 snapshot
+file_path = '/home/albertovth/SPI/spi_global_rankings_intl_25_5_2021.csv'
+spi_538_df = pd.read_csv(file_path)
 
 # Filter to include only CONMEBOL, UEFA, CONCACAF, AFC, CAF and OFC teams
 confederations = ['CONMEBOL', 'UEFA', 'CONCACAF','AFC','CAF','OFC']
@@ -15,4 +15,4 @@ spi_538_df = spi_538_df.rename(columns={'name': 'team'})
 # Save it as the initial spi_final.csv
 spi_538_df.to_csv('/home/albertovth/SPI/spi_final.csv', index=False)
 
-print("538 SPI data saved as initial spi_final.csv")
+print("SPI data from May 25, 2021 saved as initial spi_final.csv")
