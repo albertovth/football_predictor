6ea677aa2ac0d0bbd3b9bb873d0697502bbf96e4
Alberto Valiente Thoresen <46265925+albertovth@users.noreply.github.com>
2024-08-02 03:33:55 +0200

    Add files via upload

--- calculate_xG_xGA.py
+++ calculate_xG_xGA.py
@@ -9,14 +9,16 @@ df = pd.read_csv(url)
 spi_df = pd.read_csv('/home/albertovth/SPI/spi_final.csv')
 spi_df = spi_df.rename(columns={'name': 'team'})
 
+cutoff_quantile_low_teams=0.07 #Calculated with calculate_low_team_cutoff.py
+
 # Calculate the median SPI, 25th percentile SPI and the minimum SPI for the three confederations combined
 median_spi = spi_df['spi'].median()
-twenty_fifth_percentile_spi = spi_df['spi'].quantile(0.25)
+cutoff_quantile_spi = spi_df['spi'].quantile(cutoff_quantile_low_teams)
 minimum_spi = spi_df['spi'].min()
 maximum_spi = spi_df['spi'].max()
 
 print(f"Median SPI: {median_spi}")
-print(f"25th Percentile SPI: {twenty_fifth_percentile_spi}")
+print(f"First Percentile SPI: {cutoff_quantile_spi}")
 print(f"Minimum SPI: {minimum_spi}")
 
 # Load the confederations data
@@ -61,15 +63,18 @@ spi_factors = spi_df.set_index('team')['spi']
 # Initialize points for xG and xGA
 points_for_xG_high = {team: 0 for team in spi_df['team']}
 points_for_xGA_high = {team: 0 for team in spi_df['team']}
+points_for_xG_low = {team: 0 for team in spi_df['team']}
+points_for_xGA_low = {team: 0 for team in spi_df['team']}
+
 
 # High SPI teams
-high_spi_teams = spi_df[spi_df['spi'] >= twenty_fifth_percentile_spi]['team'].tolist()
+high_spi_teams = spi_df[spi_df['spi'] >= cutoff_quantile_spi]['team'].tolist()
 
 # Low SPI teams
-low_spi_teams = spi_df[spi_df['spi'] < twenty_fifth_percentile_spi]['team'].tolist()
+low_spi_teams = spi_df[spi_df['spi'] < cutoff_quantile_spi]['team'].tolist()
 
 # Define the functions to calculate points
-def calculate_points_high(row, median_spi, twenty_fifth_percentile_spi):
+def calculate_points_high(row, maximum_spi, minimum_spi):
     home_team = row['home_team']
     away_team = row['away_team']
     
@@ -95,13 +100,26 @@ def calculate_points_high(row, median_spi, twenty_fifth_percentile_spi):
     points_for_xG_high[away_team] += max(adjusted_away_goals, 0.01)
     points_for_xGA_high[away_team] += max(adjusted_home_goals, 0.01)
 
-# Apply the functions to calculate points
-filtered_df.apply(lambda row: calculate_points_high(row, median_spi, twenty_fifth_percentile_spi), axis=1)
+def calculate_points_low(row, cutoff_quantile_spi):
+    home_team = row['home_team']
+    away_team = row['away_team']
+    
+    if home_team not in points_for_xG_low or away_team not in points_for_xG_low:
+        return
+    
+    # Cap points per match at 6
+       
+    points_for_xG_low[home_team] += min(row['home_score']*0.17, 6)
+    points_for_xGA_low[home_team] += min(row['away_score']*6, 6)
+    points_for_xG_low[away_team] += min(row['away_score']*0.17, 6)
+    points_for_xGA_low[away_team] += min(row['home_score']*6, 6)
 
+# Apply the functions to calculate points
+filtered_df.apply(lambda row: calculate_points_high(row, maximum_spi, minimum_spi) if row['home_team'] in high_spi_teams and row['away_team'] in high_spi_teams else calculate_points_low(row, cutoff_quantile_spi), axis=1)
 
 # Combine the points
-points_for_xG = {team: points_for_xG_high[team]  for team in spi_df['team']}
-points_for_xGA = {team: points_for_xGA_high[team] for team in spi_df['team']}
+points_for_xG = {team: points_for_xG_high[team] + points_for_xG_low[team] for team in spi_df['team']}
+points_for_xGA = {team: points_for_xGA_high[team] + points_for_xGA_low[team] for team in spi_df['team']}
 
 # Calculate matches played
 matches_played = filtered_df['home_team'].value_counts() + filtered_df['away_team'].value_counts()
